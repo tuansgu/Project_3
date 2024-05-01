f9deb49278850c5fa8b6cb1413fb7c36faa5884c
tuansgu <nguyentuan01082003t@gmail.com>
2024-05-01 13:16:44 +0700

    Fix: entity thongtinsd

--- src/main/java/com/example/qltv/entity/Thongtinsd.java
+++ src/main/java/com/example/qltv/entity/Thongtinsd.java
@@ -1,19 +1,5 @@
 package com.example.qltv.entity;
 
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-
-import java.sql.Timestamp;
-
-@Entity
-@Table(name = "thongtinsd")
 public class Thongtinsd {
     
 }
