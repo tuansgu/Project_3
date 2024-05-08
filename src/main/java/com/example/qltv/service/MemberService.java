489b45e4e80b75ed84f798fabc57cd1a59f8eac7
PQDuy <141241165+PQDuy@users.noreply.github.com>
2024-05-08 09:29:36 +0700

    Add files via upload

--- src/main/java/com/example/qltv/service/MemberService.java
+++ src/main/java/com/example/qltv/service/MemberService.java
@@ -1,10 +1,31 @@
 package com.example.qltv.service;
 
+import com.example.qltv.DTO.LoginDTO;
 import com.example.qltv.entity.Member;
+import com.example.qltv.response.LoginMesage;
+
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public interface MemberService {
-    List<Member> getAllMember();
+    // public List<Member> getAllMember();
+
+    // public Member login(int mssv, String password);
+
+    // public boolean logout(String email);
+
+    public Member addMember(Member member);
+
+    // public boolean updatePassword(String email, String password);
+
+    // public boolean isEmailExist(String email);
+
+    // public boolean isIdExist(int id);
+
+    // public Optional<Member> findByEmal(String email);
+
+    // LoginMesage loginMember(LoginDTO loginDTO);
 }
