fd4e32aece28b2c9bedb5b3200be1791654984a4
PQDuy <141241165+PQDuy@users.noreply.github.com>
2024-05-08 09:28:06 +0700

    Add files via upload

--- src/main/java/com/example/qltv/repository/MemberRepository.java
+++ src/main/java/com/example/qltv/repository/MemberRepository.java
@@ -1,10 +1,19 @@
 package com.example.qltv.repository;
 
 import com.example.qltv.entity.Member;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
+@EnableJpaRepositories
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Integer> {
-    
+  @Query("SELECT m FROM Member m WHERE m.MaTV = :MaTV AND m.Password = :Password")
+  Optional<Member> findOneByIdAndPassword(int MaTV, String Password);
+
+  // Member findById(int MaTV);
 }
